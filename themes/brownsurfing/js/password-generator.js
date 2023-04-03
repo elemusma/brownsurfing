4697d1b93129d521840cf534cf3d39c3e76e557e
Efrain Lemus <efraintlemusm@gmail.com>
2023-04-03 05:51:22 -0600

    style random password generator

--- themes/brownsurfing/js/password-generator.js
+++ themes/brownsurfing/js/password-generator.js
@@ -9,10 +9,17 @@ function genPassword() {
         password += chars.substring(randomNumber, randomNumber + 1);
     }
     document.getElementById("password").value = password;
-}
-function copyPassword() {
+
     var copyText = document.getElementById("password");
     copyText.select();
     copyText.setSelectionRange(0, 999);
     document.execCommand("copy");
+
 }
+
+// function copyPassword() {
+//     var copyText = document.getElementById("password");
+//     copyText.select();
+//     copyText.setSelectionRange(0, 999);
+//     document.execCommand("copy");
+// }
