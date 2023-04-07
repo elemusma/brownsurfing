d9f73dde43a25386fa28bf2944dc63e73678b99c
Efrain Lemus <efraintlemusm@gmail.com>
2023-04-07 17:31:14 -0600

    start test carousel

--- themes/brownsurfing/templates/carousel.php
+++ themes/brownsurfing/templates/carousel.php
@@ -8,101 +8,148 @@
 
  ?>
  <style>
-    .gallery-container {
+
+.custom-carousel {
+  position: relative;
   width: 100%;
   overflow: hidden;
-  white-space: nowrap;
 }
 
-.gallery-container img {
-  display: inline-block;
-  width: 50vw;
-  height: 200px;
-  /* margin-right: 10px; */
+.custom-carousel-container {
+  display: flex;
+  flex-wrap: nowrap;
+  animation: scroll 10s infinite linear;
+  animation-play-state: paused;
 }
+
+.custom-carousel-item {
+  flex: 0 0 100%;
+}
+
+.custom-carousel-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 30px;
+  height: 30px;
+  font-size: 20px;
+  border: none;
+  background: transparent;
+  color: white;
+  cursor: pointer;
+}
+
+.custom-carousel-button-left {
+  left: 10px;
+}
+
+.custom-carousel-button-right {
+  right: 10px;
+}
+
+
  </style>
 
  <?php
 
- echo '<div class="gallery-container">';
-  echo wp_get_attachment_image(1847,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
+//  echo '<div class="container">';
+ echo '<div class="custom-carousel">';
+ echo '<div class="custom-carousel-container">';
+
+echo '<div class="custom-carousel-item">';
+echo wp_get_attachment_image(1847,'full','',[
+  'class'=>'',
+  'style'=>'height:400px;object-fit:cover;object-position:top;',
+  'onerror'=>'console.error("Error loading image")'
+]);
+echo '</div>';
+
+echo '<div class="custom-carousel-item">';
   echo wp_get_attachment_image(1754,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1542,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1847,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1754,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1542,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1847,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1754,'full','',[
-    'class'=>'',
-    'style'=>'height:400px;object-fit:cover;object-position:top;'
-  ]);
-  echo wp_get_attachment_image(1542,'full','',[
     'class'=>'',
     'style'=>'height:400px;object-fit:cover;object-position:top;'
   ]);
 echo '</div>';
 
-echo '<button id="scroll-left">Previous</button>';
-echo '<button id="scroll-right">Next</button>';
+echo '<div class="custom-carousel-item">';
+  echo wp_get_attachment_image(1542,'full','',[
+    'class'=>'custom-carousel-item',
+    'style'=>'height:400px;object-fit:cover;object-position:top;'
+  ]);
+echo '</div>';
 
+
+echo '</div>';
+
+// Add more items as needed
+
+  echo '<button class="custom-carousel-button custom-carousel-button-left">&lt;</button>';
+  echo '<button class="custom-carousel-button custom-carousel-button-right">&gt;</button>';
+
+
+echo '</div>';
 ?>
 
 <script>
-    const galleryContainer = document.querySelector('.gallery-container');
-const scrollLeftButton = document.querySelector('#scroll-left');
-const scrollRightButton = document.querySelector('#scroll-right');
 
-let scrollAmount = 0;
+window.addEventListener('load', function() {
+  var container = document.querySelector('.carousel-container');
+  var firstItem = container.querySelector('.custom-carousel-item');
+  var clonedItem = firstItem.cloneNode(true);
+  var containerWidth = container.offsetWidth * 2;
 
-function scrollGallery(direction) {
-  const galleryWidth = galleryContainer.offsetWidth;
-  const images = galleryContainer.querySelectorAll('img');
-  const imagesTotalWidth = images.length * images[0].offsetWidth;
-  const minScrollAmount = 0;
-  const maxScrollAmount = imagesTotalWidth - galleryWidth;
+  container.appendChild(clonedItem);
+  container.style.width = containerWidth + 'px';
+  container.style.animation = 'scroll 10s infinite linear';
+  container.style.animationPlayState = 'paused';
 
-  if (direction === 'left') {
-    scrollAmount -= window.innerWidth/2;
-    if (scrollAmount < minScrollAmount) {
-      scrollAmount = maxScrollAmount;
-    }
-  } else if (direction === 'right') {
-    scrollAmount += window.innerWidth/2;
-    if (scrollAmount > maxScrollAmount) {
-      scrollAmount = minScrollAmount;
-    }
-  }
+  // Add left and right buttons
+  var leftButton = document.createElement('button');
+  leftButton.textContent = '<';
+  leftButton.classList.add('custom-carousel-button', 'custom-carousel-button-left');
+  container.parentElement.appendChild(leftButton);
 
-  galleryContainer.scrollTo({
-    top: 0,
-    left: scrollAmount,
-    behavior: 'smooth'
+  var rightButton = document.createElement('button');
+  rightButton.textContent = '>';
+  rightButton.classList.add('custom-carousel-button', 'custom-carousel-button-right');
+  container.parentElement.appendChild(rightButton);
+
+  // Start the animation when page is fully loaded
+  window.addEventListener('load', function() {
+    container.style.animationPlayState = 'running';
   });
-}
 
-scrollLeftButton.addEventListener('click', () => scrollGallery('left'));
-scrollRightButton.addEventListener('click', () => scrollGallery('right'));
+  // Pause the animation on mouseover
+  container.addEventListener('mouseover', function() {
+    container.style.animationPlayState = 'paused';
+  });
+
+  // Resume the animation on mouseout
+  container.addEventListener('mouseout', function() {
+    container.style.animationPlayState = 'running';
+  });
+
+  // Scroll left on left button click
+  leftButton.addEventListener('click', function() {
+    var currentPosition = parseInt(container.style.transform.split('(')[1]);
+    if (currentPosition === 0) {
+      container.style.transform = 'translateX(-' + (containerWidth - container.offsetWidth) + 'px)';
+    } else {
+      container.style.transform = 'translateX(' + currentPosition + 'px)';
+    }
+  });
+
+  // Scroll right on right button click
+  rightButton.addEventListener('click', function() {
+    var currentPosition = parseInt(container.style.transform.split('(')[1]);
+    if (currentPosition === -(containerWidth - container.offsetWidth)) {
+      container.style.transform = 'translateX(0px)';
+    } else {
+      container.style.transform = 'translateX(' + (currentPosition - container.offsetWidth) + 'px)';
+    }
+  });
+});
+
 
 </script>
 
